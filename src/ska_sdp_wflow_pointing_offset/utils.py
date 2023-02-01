bbb7690cc4c7582c4961b916132f3a9aecf5aef7
luminosa42 <yl790@cam.ac.uk>
2023-02-01 22:56:33 +0000

    ORC-1566: Delete one function

--- src/ska_sdp_wflow_pointing_offset/utils.py
+++ src/ska_sdp_wflow_pointing_offset/utils.py
@@ -1,22 +1,3 @@
-# pylint: disable=invalid-name
 """
 Util functions for plotting
 """
-
-import matplotlib.pyplot as plt
-
-
-def plot_azel(az, el):
-    """
-    Plot az, el in degrees
-    :param az:
-    :param el:
-    :return:
-    """
-
-    plt.figure(figsize=(10, 5))
-    plt.plot(az, el, "bo")
-    plt.xlabel("Azimuth [degrees]")
-    plt.ylabel("Elevation [degrees]")
-    plt.title("Pointing offset information")
-    plt.savefig("azel.png")
