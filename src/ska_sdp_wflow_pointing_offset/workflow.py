5dfeb31930dfe03fd608eba8d8c331bdb05d5525
Johannes Allotey <ja784@cam.ac.uk>
2023-02-13 13:22:19 +0000

    ORC-1586: Extracted additional parameters (visibility weights etc)

--- src/ska_sdp_wflow_pointing_offset/workflow.py
+++ src/ska_sdp_wflow_pointing_offset/workflow.py
@@ -11,7 +11,7 @@ import numpy
 def apply_rfi_mask(data, freqs, rfi_filename=None):
     """
     Apply RFI mask.
-    :param data: 3D data in [ncorr, nchan, numpyol]
+    :param data: 3D data in [ncorr, nchan, npol]
     :param freqs: 1D array of frequency in Hz [nchan]
     :param rfi_filename: Name of the rfi pickle file
     :return: filtered data and freqs array
@@ -48,6 +48,7 @@ def clean_vis_data(
     vis_array,
     freqs,
     corr_type,
+    vis_weight,
     start_freq=None,
     end_freq=None,
     apply_mask=False,
@@ -57,10 +58,11 @@ def clean_vis_data(
     """
     Clean visibility data and split into polarisations.
 
-    :param vis_array: Numpy array of visibility data [ncorr, nchan, numpyol]
+    :param vis_array: Numpy array of visibility data [ncorr, nchan, npol]
     :param freqs: Numpy array of frequency [nchan]
     :param corr_type: Correlation type e.g. (XX,YY), (RR, LL),
                         (XX,XY,YX,YY) or (RR,RL,LR,LL)
+    :param vis_weight: Weights of the visibilities [ncorr, npol]
     :param start_freq: Starting frequency for selection in MHz
                        If no selection needed, use None
     :param end_freq: Ending frequency for selection in MHz
@@ -70,7 +72,7 @@ def clean_vis_data(
     :param split_pol: Split polarisations?
     :return: numpy array of visibility with shape [ncorr,]
              If split_pol is True, return each polarisation
-             Else return as one array
+             Else return the polarisation-averaged visibilities
     """
 
     # Use amplitude for visibility
@@ -89,27 +91,40 @@ def clean_vis_data(
         start_freq = filtered_freq[0]
     if end_freq is None:
         end_freq = filtered_freq[-1]
-    selected_vis, _ = select_channels(
+    selected_vis, selected_freq = select_channels(
         filtered_vis, filtered_freq, start_freq, end_freq
     )
 
     # Average over frequency
     avg_vis = numpy.mean(selected_vis, axis=1)
-
-    # Split polarisations
     if split_pol:
-        # Split into the parallel hands
+        # Split into each parallel hand visibilities
         if len(corr_type) == 2:
             # (XX,YY) or (RR, LL)
+            corr_type = [corr_type[0], corr_type[1]]
             vis_h = avg_vis[:, 0]
             vis_v = avg_vis[:, 1]
+            weight_h = vis_weight[:, 0]
+            weight_v = vis_weight[:, 1]
         elif len(corr_type) == 4:
             # (XX,XY,YX,YY) or (RR,RL,LR,LL)
+            corr_type = [corr_type[0], corr_type[3]]
             vis_h = avg_vis[:, 0]
             vis_v = avg_vis[:, 3]
+            weight_h = vis_weight[:, 0]
+            weight_v = vis_weight[:, 3]
         else:
             raise ValueError("Polarisation type not supported")
 
-        return vis_h, vis_v
+        return numpy.array(
+            [[vis_h, vis_v], selected_freq, [weight_h, weight_v], corr_type]
+        )
 
-    return avg_vis
+    return numpy.array(
+        [
+            numpy.mean(avg_vis, axis=1),
+            selected_freq,
+            numpy.mean(vis_weight, axis=1),
+            corr_type,
+        ]
+    )  # how do we deal with the beam squint?
