7def2f309de8b0c159dbe7921dbacfc4d37de362
Johannes Allotey <ja784@cam.ac.uk>
2023-02-14 09:56:51 +0000

    ORC-1586: Beam fitting to cross-correlation visibilities implemented

--- src/ska_sdp_wflow_pointing_offset/beam_fitting.py
+++ src/ska_sdp_wflow_pointing_offset/beam_fitting.py
@@ -5,9 +5,11 @@ computing the azimuth and elevation offsets. This follows the
 routines used by the SARAO team for the MeerKAT array.
 """
 
-import katpoint
 import numpy
+from katpoint import lightspeed, wrap_angle
 from scikits.fitting import GaussianFit, ScatterFit
+
+from src.ska_sdp_wflow_pointing_offset.coord_support import convert_coordinates
 
 
 def fwhm_to_sigma(fwhm):
@@ -162,20 +164,18 @@ def fit_primary_beams(
     # Compute the primary beam size for use as initial parameter of the Gaussian
     # Use higher end of the frequency band with smallest beam for better pointing accuracy
     # Convert power beamwidth to gain / voltage beamwidth
-    wavelength = numpy.degrees(katpoint.lightspeed / freqs[-1])
+    wavelength = numpy.degrees(lightspeed / freqs[-1])
     expected_width = numpy.sqrt(2.0) * (
         beamwidth_factor * wavelength / dish_diameter
     )
 
     # XXX This assumes we are still using default beamwidth factor of 1.22
     # and also handles larger effective dish diameter in H direction. Note that the comment
-    # applies to the MeerKAT
+    # applies to the MeerKAT but would that apply to the SKA ?
     expected_width = (0.8 * expected_width, 0.9 * expected_width)
     fitted_beam = BeamPatternFit(
         center=(0.0, 0.0), width=expected_width, height=1.0
     )
-
-    print(dish_coordinates.shape)
 
     # TO Do: Test primary beam fitting to the cross-correlation visibilities
     if auto:
@@ -183,21 +183,36 @@ def fit_primary_beams(
         print("Fitting primary beams to auto-correlation visibilities")
     else:
         # Split number of correlations -> (timestamps, baseline) for per baseline fitting
-        raise NotImplementedError(
-            "Beam fitting to cross-correlation visibilities not implemented!"
-        )
+        print("Fitting primary beams to cross-correlation visibilities")
 
     # Begin fitting the primary beam to the visibilities
     if split_pol:
         # Fit to the visibilities of each polarisation
         for vis, weight, corr in zip(avg_vis, vis_weight, corr_type):
             print(f"\nFitting of primary beams to {corr}")
-            vis = vis.reshape(
-                len(timestamps), int(vis.shape[0] / len(timestamps))
-            )
-            weight = weight.reshape(
-                len(timestamps), int(weight.shape[0] / len(timestamps))
-            )
+            if auto:
+                vis = vis.reshape(
+                    len(timestamps), int(vis.shape[0] / len(timestamps))
+                )
+                weight = weight.reshape(
+                    len(timestamps), int(weight.shape[0] / len(timestamps))
+                )
+            else:
+                # Since the x parameter required for the fitting has shape (2, number of timestamps, number of antennas),
+                # we need to find a way to reshape the cross-correlation visibilities to include number of antennas instead of baselines.
+                # Is this the correct way to do it?
+                vis = vis.reshape(
+                    len(timestamps),
+                    int(vis.shape[0] / len(timestamps) / len(ants)),
+                    len(ants),
+                )
+                weight = weight.reshape(
+                    len(timestamps),
+                    int(weight.shape[0] / len(timestamps) / len(ants)),
+                    len(ants),
+                )
+                vis = numpy.mean(vis, axis=1)
+                weight = numpy.mean(weight, axis=1)
             for i in range(vis.shape[1]):
                 print(
                     f"Fitting primary beam to visibilities of Antenna {ants[i].name}"
@@ -211,22 +226,64 @@ def fit_primary_beams(
                     fitted_beam.center / fitted_beam.std_center
                 )
                 width_norm = numpy.radians(fitted_beam.center / expected_width)
-                print(
-                    f"Centre=({center_norm[0]:.4f},{center_norm[1]:.4f}) +/- ({center_norm[0]}), "
-                    f"Width={width_norm[0]:.4f}+/-{width_norm[1]:.4f}"
-                )
+
+                # Convert the fitted beam centre from (x,y) to (az,el)
+                try:
+                    fitted_az, fitted_el = convert_coordinates(
+                        ant=ants[i],
+                        beam_center=center_norm,
+                        timestamps=timestamps,
+                        target_projection=target_projection,
+                        target_object=target,
+                    )
+                    requested_az, requested_el = target.azel(
+                        timestamp=numpy.median(timestamps), antenna=ants[i]
+                    )
+                    fitted_az, fitted_el = numpy.degrees(
+                        fitted_az
+                    ), numpy.degrees(fitted_el)
+                    requested_az, requested_el = numpy.degrees(
+                        requested_az
+                    ), numpy.degrees(requested_el)
+                    offset_az, offset_el = wrap_angle(
+                        fitted_az - requested_az, 360.0
+                    ), wrap_angle(fitted_el - requested_el, 360.0)
+                    # print(
+                    #    f"Centre=({center_norm[0]:.8f},{center_norm[1]:.8f}), Width=({width_norm[0]:.8f},{width_norm[1]:.8f})"
+                    # )
+                    print(offset_az, offset_el)
+                except:
+                    print(f"No valid primary beam fit for {ants[i].name}")
     else:
         # Fit to the frequency-polarisation-averaged visibilities
-        avg_vis = avg_vis.reshape(
-            len(timestamps), int(avg_vis.shape[0] / len(timestamps))
-        )
-        vis_weight = vis_weight.reshape(
-            len(timestamps), int(vis_weight.shape[0] / len(timestamps))
-        )
-        for i in range(avg_vis.shape[1]):
-            print(
-                f"\nFitting primary beam to visibilities of Antenna {ants[i].name}"
+        if auto:
+            avg_vis = avg_vis.reshape(
+                len(timestamps), int(avg_vis.shape[0] / len(timestamps))
             )
+            vis_weight = vis_weight.reshape(
+                len(timestamps), int(vis_weight.shape[0] / len(timestamps))
+            )
+        else:
+            # Since the x parameter required for the fitting has shape (2, number of timestamps, number of antennas),
+            # we need to find a way to reshape the cross-correlation visibilities to include number of antennas instead of baselines.
+            # Is this the correct way to do it?
+            avg_vis = avg_vis.reshape(
+                len(timestamps),
+                int(avg_vis.shape[0] / len(timestamps) / len(ants)),
+                len(ants),
+            )
+            vis_weight = vis_weight.reshape(
+                len(timestamps),
+                int(vis_weight.shape[0] / len(timestamps) / len(ants)),
+                len(ants),
+            )
+            avg_vis = numpy.mean(avg_vis, axis=1)
+            vis_weight = numpy.mean(vis_weight, axis=1)
+
+        for i in range(avg_vis.shape[1]):
+            # print(
+            #    f"\nFitting primary beam to visibilities of Antenna {ants[i].name}"
+            # )
             fitted_beam.fit(
                 x=dish_coordinates[:, :, i],
                 y=avg_vis[:, i],
@@ -236,7 +293,32 @@ def fit_primary_beams(
                 fitted_beam.center / fitted_beam.std_center
             )
             width_norm = numpy.radians(fitted_beam.center / expected_width)
-            print(
-                f"Centre=({center_norm[0]:.4f},{center_norm[1]:.4f}) +/- ({center_norm[0]}), "
-                f"Width={width_norm[0]:.4f}+/-{width_norm[1]:.4f}"
-            )
+
+            # Convert the fitted beam centre from (x,y) to (az,el)
+            try:
+                fitted_az, fitted_el = convert_coordinates(
+                    ant=ants[i],
+                    beam_center=center_norm,
+                    timestamps=timestamps,
+                    target_projection=target_projection,
+                    target_object=target,
+                )
+                requested_az, requested_el = target.azel(
+                    timestamp=numpy.median(timestamps), antenna=ants[i]
+                )
+
+                fitted_az, fitted_el = numpy.degrees(fitted_az), numpy.degrees(
+                    fitted_el
+                )
+                requested_az, requested_el = numpy.degrees(
+                    requested_az
+                ), numpy.degrees(requested_el)
+                offset_az, offset_el = wrap_angle(
+                    fitted_az - requested_az, 360.0
+                ), wrap_angle(fitted_el - requested_el, 360.0)
+                # print(
+                #    f"Centre=({center_norm[0]:.8f},{center_norm[1]:.8f}), Width=({width_norm[0]:.8f},{width_norm[1]:.8f})"
+                # )
+                print(offset_az, offset_el)
+            except:
+                print(f"\nNo valid primary beam fit for {ants[i].name}")
