6b352f725a09c2f489b23c3195822d69c0176b44
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2023-02-15 16:57:13 +0000

    ORC-1586 Updating with recent changes

--- src/ska_sdp_wflow_pointing_offset/pointing_offset_cli.py
+++ src/ska_sdp_wflow_pointing_offset/pointing_offset_cli.py
@@ -1,33 +1,29 @@
-"""
-Command line utility for Pointing Offset Calibration Pipeline.
+"""Example of program with many options using docopt.
 
 Usage:
-    pointing-offset COMMAND [OBJECTS] [<args>...]
-    pointing-offset COMMAND (-h|--help)
-    pointing-offset (-h|--help)
-
-Objects:
-    ms      Measurement set file
-    rdb     RDB file
+  pointing-offset COMMAND [--ms=FILE] [--rdb=FILE] [--save_offset=False] [--results_dir=None] [--start_freq=None] [--end_freq=None]
+  pointing-offset --version
 
 Commands:
-    read        Read file
-    clean       Clean Visibility Data
+  compute   Compute all calculations
+
+Options:
+  -h --help            show this help message and exit
+  -q --quiet           report only file names
 
 """
-
 import logging
 import sys
 
 from docopt import docopt
 
-from .read_data import (
-    read_cross_correlation_visibilities,
+from ska_sdp_wflow_pointing_offset.beam_fitting import fit_primary_beams
+from ska_sdp_wflow_pointing_offset.coord_support import convert_coordinates
+from ska_sdp_wflow_pointing_offset.read_data import (
     read_data_from_rdb_file,
+    read_visibilities,
 )
-from .coord_support import convert_coordinates
-
-from .workflow import clean_vis_data
+from src.ska_sdp_wflow_pointing_offset.workflow import clean_vis_data
 
 LOG = logging.getLogger("ska-sdp-pointing-offset")
 LOG.setLevel(logging.INFO)
@@ -36,20 +32,22 @@ LOG.addHandler(logging.StreamHandler(sys.stdout))
 COMMAND = "COMMAND"
 
 
-def main(argv=None):
+def main():
     """Run ska-sdp."""
-    if argv is None:
-        argv = sys.argv[1:]
 
-    args = docopt(__doc__, argv=argv, options_first=True)
+    args = docopt(__doc__)
 
-    if args[COMMAND] == "read":
-        read_clean_data(argv)
 
-    # This doesn't work properly now but need to decide
-    # what to do with it
-    if args[COMMAND] == "clean":
-        clean_vis_data(argv)
+
+
+
+    if args[COMMAND] == "compute":
+        print("I am in here!!!")
+        if args['--ms'] and args['--rdb']:
+            compute_everything(args)
+        else:
+            raise ValueError("MS and RDB are required!!")
+
 
     else:
         LOG.error(
@@ -59,22 +57,58 @@ def main(argv=None):
         )
 
 
-# Just the first iteration, will split into seperate once
-# we have a good idea about the cli
-def read_clean_data(argv):
-    """Read MS file and run the clean visibilita data"""
+def compute_everything(args):
+    """."""
 
-    args = docopt(__doc__, argv=argv)
-    if args['OBJECTS'] == 'ms':
-        print("Just for testing MS!")
-        vis, freqs, corr_type = read_cross_correlation_visibilities(args['<args>'][0])
-        clean_vis_data(vis, freqs, corr_type)
-    elif args['OBJECTS'] == 'rdb':
-        print("Just for testing RDB!")
-        az, el, timestamps, target_projection, ants, target = read_data_from_rdb_file(args['<args>'][0])
+    # Get visibilities
+    vis, freqs, corr_type, dish_diam, vis_weight = read_visibilities(
+        msname=args.msname, auto=args.auto
+    )
 
-        # Need to add beam centre which will come from beam fitting script
-        convert_coordinates(ants, beam_centre, timestamps, target_projection)
+    # Get the metadata
+    (
+        az,
+        el,
+        timestamps,
+        target_projection,
+        ants,
+        target,
+        dish_coord,
+    ) = read_data_from_rdb_file(rdbfile=args.metadata, auto=args.auto)
+
+    # Get RFI-free visibilities
+    avg_vis, selected_freqs, vis_weight, corr_type = clean_vis_data(
+        vis,
+        freqs,
+        corr_type,
+        vis_weight=vis_weight,
+        start_freq=args.start_freq,
+        end_freq=args.end_freq,
+        apply_mask=args.apply_mask,
+        rfi_filename=args.rfi_file,
+        split_pol=args.split_pol,
+    )
+
+    # Fit primary beams to visibilities
+    fit_primary_beams(
+        avg_vis=avg_vis,
+        freqs=freqs,
+        timestamps=timestamps,
+        corr_type=corr_type,
+        vis_weight=vis_weight,
+        ants=ants,
+        dish_diameter=dish_diam,
+        dish_coordinates=dish_coord,
+        target=target,
+        target_projection=target_projection,
+        beamwidth_factor=ants[0].beamwidth,
+        auto=args.auto,
+        split_pol=args.split_pol,
+        save_offset=true,
+        # Optional - Don't need it when it is None
+        results_dir=None,
+    )
+
 
 if __name__ == "__main__":
     main()
