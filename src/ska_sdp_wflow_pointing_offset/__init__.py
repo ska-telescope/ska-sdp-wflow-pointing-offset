bb7c95ac6d268b3ca4f86ac0f0dd06c1f702774a
Johannes Allotey <ja784@cam.ac.uk>
2023-02-21 20:42:21 +0000

    ORC-1574: Fixed linting

--- src/ska_sdp_wflow_pointing_offset/__init__.py
+++ src/ska_sdp_wflow_pointing_offset/__init__.py
@@ -2,11 +2,13 @@
 Init file
 """
 
+
+from .beam_fitting import fit_primary_beams
 from .coord_support import construct_antennas, convert_coordinates
 from .export_data import export_pointing_offset_data
 from .freq_select import apply_rfi_mask, clean_vis_data, select_channels
+from .pointing_offset_cli import compute_offset
 from .read_data import read_data_from_rdb_file, read_visibilities
-from .utils import plot_azel
 
 __all__ = [
     "export_pointing_offset_data",
@@ -18,6 +20,5 @@ __all__ = [
     "construct_antennas",
     "convert_coordinates",
     "fit_primary_beams",
-    "compute_offset"
-    "plot_azel",
+    "compute_offset",
 ]
