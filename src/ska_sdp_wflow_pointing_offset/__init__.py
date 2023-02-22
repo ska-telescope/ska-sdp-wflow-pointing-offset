393123ec078d70b26ce0bc5b22c8c94960734373
Johannes Allotey <ja784@cam.ac.uk>
2023-02-22 07:16:51 +0000

    ORC-1574: Reversed additions to __init__ file which causes processing to be done twice

--- src/ska_sdp_wflow_pointing_offset/__init__.py
+++ src/ska_sdp_wflow_pointing_offset/__init__.py
@@ -3,11 +3,9 @@ Init file
 """
 
 
-from .beam_fitting import fit_primary_beams
 from .coord_support import construct_antennas, convert_coordinates
 from .export_data import export_pointing_offset_data
 from .freq_select import apply_rfi_mask, clean_vis_data, select_channels
-from .pointing_offset_cli import compute_offset
 from .read_data import read_data_from_rdb_file, read_visibilities
 
 __all__ = [
@@ -19,6 +17,4 @@ __all__ = [
     "clean_vis_data",
     "construct_antennas",
     "convert_coordinates",
-    "fit_primary_beams",
-    "compute_offset",
 ]
