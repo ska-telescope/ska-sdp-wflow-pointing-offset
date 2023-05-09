e8a889a5fc9665f8a5c5cd77cb24c4d57732be27
Johannes Allotey <ja784@cam.ac.uk>
2023-05-09 10:46:51 +0100

    ORC-1634: Refactored tests

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -7,27 +7,20 @@ from unittest.mock import patch
 import numpy
 import pytest
 
-from tests.utils import (
-    ANTS,
-    CORR_TYPE,
-    DISH_COORD_AZ,
-    DISH_COORD_EL,
-    FREQS,
-    VIS_ARRAY,
-)
+from tests.utils import CORR_TYPE, FREQS
 
 casacore = pytest.importorskip("casacore")
 
 
 @patch("ska_sdp_wflow_pointing_offset.read_data.read_visibilities")
-def test_read_visibilities(read_visibilities):
+def test_read_visibilities(read_visibilities, ants, vis_array, source_offset):
     """
     Unit test for read_visibilities function
     """
     read_visibilities.return_value = (
-        VIS_ARRAY,
-        numpy.dstack((DISH_COORD_AZ, DISH_COORD_EL)),
-        ANTS,
+        vis_array,
+        source_offset,
+        ants,
     )
     vis, source_offset, ants = read_visibilities("test_table")
 
